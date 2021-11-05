e0a4f0b9ca2921c55a9405d0777df40182289c3e
unknown <widya160499@gmail.com>
2021-11-05 23:06:01 +0800

    update css

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -58,7 +58,7 @@
                 <div class="col-lg-4 col-md-4 col-sm-12">
                     <div class="card card-statistic-2">
                         <div class="card-icon shadow-primary bg-primary">
-                            <i class="fas fa-piggy-bank"></i>
+                            <i class="fas fa-chart-bar"></i>
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">

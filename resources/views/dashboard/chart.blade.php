12f92da1515bbd749aff46300fe90d50bf50aa59
Bali Merta <made.b4li@gmail.com>
2021-11-08 10:33:09 +0800

    Update chart.blade.php

--- resources/views/dashboard/chart.blade.php
+++ resources/views/dashboard/chart.blade.php
@@ -19,7 +19,7 @@
 
 
 <div class="row">
-    <div class="col-12 col-md-6 d-flex">
+    <div class="col-12 {{ userRole() == 'pegawai' ? 'col-md-12' : 'col-md-6' }} d-flex">
         @include('dashboard.chart_parts.empty_room')
     </div>
     
